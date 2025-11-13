65e4d4d53d70d1ee8631b542d57c5527592cf66b
gulmirze101 <gulmirzata-map101@code.edu.az>
2025-11-13 10:10:41 +0400

    Add files via upload

---
+++ Task3.cs
@@ -0,0 +1,28 @@
+﻿LogMessage("Salam salam", SimpleLogger);
+LogMessage("Salam salam", TimeLogger);
+LogMessage("Salam salam", UpperLogger);
+LogMessage("Salam salam", FileLogger);
+void LogMessage(string text, Logger logger)
+{
+    logger(text);
+}
+void SimpleLogger(string text)
+{
+    Console.WriteLine(text);
+}
+void TimeLogger(string text)
+{
+    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {text}");
+}
+
+void UpperLogger(string text)
+{
+    Console.WriteLine(text.ToUpper());
+}
+
+void FileLogger(string text)
+{
+    Console.WriteLine($"(Fayla yazıldı): {text}");
+}
+
+delegate void Logger(string text);
